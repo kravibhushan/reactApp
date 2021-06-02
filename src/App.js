716efa79aa57ffbc3211c5f2710a504ab192484e
Ravi Bhushan Kumar <ravibhushan.kumar@outlook.com>
2021-06-02 22:20:51 +0200

    New component add just for testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import Employee from ".componenets/employee";
 import "./App.css";
 
 function App() {
+  Employee
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ravi Here for Git demo
-        </a>
-      </header>
+     This is from Main component
+     <Employee></Employee>
     </div>
   );
 }
---
+++ src/componenets/employee.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Employee() {
+  return (
+    <div className="App">
+     This is from Employee component
+    </div>
+  );
+}
+
+export default Employee;
